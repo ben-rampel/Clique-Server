a6e5d11444dcd6048f34664f48bdeb50ebcca1dc
Ben <brampel@live.unc.edu>
2019-10-12 12:25:18 -0400

    created data model, started templating api

---
+++ src/main/java/com/fordexplorer/clique/Person.java
@@ -0,0 +1,75 @@
+package com.fordexplorer.clique;
+
+import java.util.List;
+
+public class Person {
+    private String username, firstName, lastName, password;
+    private List<String> interests;
+    private String bio;
+    private String phoneNumber;
+    private Group currentGroup;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<String> getInterests() {
+        return interests;
+    }
+
+    public void setInterests(List<String> interests) {
+        this.interests = interests;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Group getCurrentGroup() {
+        return currentGroup;
+    }
+
+    public void setCurrentGroup(Group currentGroup) {
+        this.currentGroup = currentGroup;
+    }
+}
---
+++ src/main/java/com/fordexplorer/clique/apiController.java
@@ -0,0 +1,37 @@
+package com.fordexplorer.clique;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class apiController {
+    /* Account management */
+
+    //Create user
+    @PostMapping("/registerUser")
+    public void registerUser(@RequestBody Person person){
+
+    }
+    //Login
+    //login(credentials) -> JWT Token
+
+    //Get user profile
+    @GetMapping("/getProfile/{username}")
+    public Person getProfile(@PathVariable String username){
+        return null;
+    }
+
+    /* Group management */
+
+    //Create group
+    @PostMapping("/createGroup")
+    public void createGroup(@RequestBody String body){
+        Group group = new Group();
+        //Add the creater of the group
+        //group.addMember();
+    }
+    //Get groups near user
+    //Get group info, including people wanting to join group
+
+    /* Group interaction */
+    //Chat
+}
