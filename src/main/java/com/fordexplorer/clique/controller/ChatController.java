828022747151312a8dfcca0a9a253b8b9a6478dd
Ben <brampel@live.unc.edu>
2019-10-12 19:52:43 -0400

    chatcontroller tweaks

--- src/main/java/com/fordexplorer/clique/controller/ChatController.java
+++ src/main/java/com/fordexplorer/clique/controller/ChatController.java
@@ -11,8 +11,6 @@ import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 import java.util.Optional;
@@ -25,6 +23,8 @@ public class ChatController {
 
     @Autowired
     public ChatController(SimpMessageSendingOperations messagingTemplate, GroupRepository groupRepository){
+        this.messagingTemplate = messagingTemplate;
+        this.groupRepository = groupRepository;
         mapper = new ObjectMapper();
     }
 
@@ -53,6 +53,5 @@ public class ChatController {
     private String chatURL(Group g){
         return String.format("/chat/%d",g.getId());
     }
-
 
 }
