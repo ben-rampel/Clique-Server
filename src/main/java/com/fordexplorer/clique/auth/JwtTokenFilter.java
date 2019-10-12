15e26289fe3975b52d4039585df0f1bc93148e5e
Shilong Dai <sdai@viperfish.net>
2019-10-12 16:38:37 -0400

    added more logging

--- src/main/java/com/fordexplorer/clique/auth/JwtTokenFilter.java
+++ src/main/java/com/fordexplorer/clique/auth/JwtTokenFilter.java
@@ -1,5 +1,7 @@
 package com.fordexplorer.clique.auth;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.GenericFilterBean;
@@ -13,6 +15,7 @@ import java.io.IOException;
 
 public class JwtTokenFilter extends GenericFilterBean {
     private JwtTokenManager jwtTokenProvider;
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     public JwtTokenFilter(JwtTokenManager jwtTokenProvider) {
         this.jwtTokenProvider = jwtTokenProvider;
@@ -22,9 +25,11 @@ public class JwtTokenFilter extends GenericFilterBean {
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain filterChain)
             throws IOException, ServletException {
         String token = jwtTokenProvider.resolveToken((HttpServletRequest) req);
+        logger.debug("Resolved token [{}] from {}", token, req.getRemoteAddr());
         if (token != null && jwtTokenProvider.validateToken(token)) {
             Authentication auth = token != null ? jwtTokenProvider.getAuthentication(token) : null;
             SecurityContextHolder.getContext().setAuthentication(auth);
+            logger.debug("Authenticated {}", jwtTokenProvider.getUsername(token));
         }
         filterChain.doFilter(req, res);
     }
